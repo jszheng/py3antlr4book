24e1fa222b594dcd7849c53430a4f88f44033ac2
jszheng <zheng_js@hotmail.com>
2016-02-17 12:16:40 +0800

    Add missing file remove one ignore pattern.

---
+++ 04-Calc/MyVisitor.py
@@ -0,0 +1,46 @@
+__author__ = 'jszheng'
+
+from LabeledExprVisitor import LabeledExprVisitor
+from LabeledExprParser import LabeledExprParser
+
+
+class MyVisitor(LabeledExprVisitor):
+    def __init__(self):
+        self.memory = {}
+
+    def visitAssign(self, ctx):
+        name = ctx.ID().getText()
+        value = self.visit(ctx.expr())
+        self.memory[name] = value
+        return value
+
+    def visitPrintExpr(self, ctx):
+        value = self.visit(ctx.expr())
+        print(value)
+        return 0
+
+    def visitInt(self, ctx):
+        return ctx.INT().getText()
+
+    def visitId(self, ctx):
+        name = ctx.ID().getText()
+        if name in self.memory:
+            return self.memory[name]
+        return 0
+
+    def visitMulDiv(self, ctx):
+        left = int(self.visit(ctx.expr(0)))
+        right = int(self.visit(ctx.expr(1)))
+        if ctx.op.type == LabeledExprParser.MUL:
+            return left * right
+        return left / right
+
+    def visitAddSub(self, ctx):
+        left = int(self.visit(ctx.expr(0)))
+        right = int(self.visit(ctx.expr(1)))
+        if ctx.op.type == LabeledExprParser.ADD:
+            return left + right
+        return left - right
+
+    def visitParens(self, ctx):
+        return self.visit(ctx.expr())
